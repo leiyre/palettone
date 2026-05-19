c0778f829165fa6003cf22f3ae0c38beb55002f9
Leire Aguirre <leireaguirre.code@gmail.com>
2026-05-19 09:42:52 +0200

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+    },
+  },
   css: ["@/assets/styles/main.scss"],
   components: [
     {
